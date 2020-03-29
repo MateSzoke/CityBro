62891556e57822c58021914f3bf716a77ca7e7b5
Szőke Máté <szoke.mate@autsoft.hu>
2020-03-29 17:20:00 +0200

    Add Room with CityBase entity

---
+++ app/src/main/java/hu/szokemate/citybro/data/db/RoomModule.kt
@@ -0,0 +1,27 @@
+package hu.szokemate.citybro.data.db
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class RoomModule {
+
+    companion object {
+        private const val DB_NAME = "citybro-db"
+    }
+
+    @Provides
+    @Singleton
+    fun provideRoomDatabase(context: Context): CityBroDatabase {
+        return Room.databaseBuilder(
+            context,
+            CityBroDatabase::class.java,
+            DB_NAME
+        ).fallbackToDestructiveMigration()
+            .build()
+    }
+
+}
--- app/src/main/java/hu/szokemate/citybro/di/AppComponent.kt
+++ app/src/main/java/hu/szokemate/citybro/di/AppComponent.kt
@@ -3,6 +3,7 @@ package hu.szokemate.citybro.di
 import co.zsmb.rainbowcake.dagger.RainbowCakeComponent
 import co.zsmb.rainbowcake.dagger.RainbowCakeModule
 import dagger.Component
+import hu.szokemate.citybro.data.db.RoomModule
 import javax.inject.Singleton
 
 
@@ -11,7 +12,8 @@ import javax.inject.Singleton
     modules = [
         RainbowCakeModule::class,
         ApplicationModule::class,
-        ViewModelModule::class
+        ViewModelModule::class,
+        RoomModule::class
     ]
 )
 interface AppComponent : RainbowCakeComponent
